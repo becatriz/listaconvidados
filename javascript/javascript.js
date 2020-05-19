9ab718ecdaea69246c961c566da17a3aa9fb4b74
Rebeca <becatriz7@gmail.com>
2020-05-18 21:17:45 -0400

    Melhorada a responsabilidade da tela

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,6 +5,10 @@ function adicionar() {
   let nomes = document.getElementById("convidado").value
   let idade = document.getElementById("idade").value
   let email = document.getElementById("email").value
+
+  if(nomes === ""){
+    return alert("Favor, preencher o nome do convidado.")
+  }
   let convidado = {
     nome: nomes,
     idade: idade,
@@ -16,22 +20,32 @@ function adicionar() {
 }
 
 function atualizar() {
-   let table = document.getElementById("table")
-   table.innerHTML = ""
-   document.getElementById("section").style.visibility = "visible"
+  let table = document.getElementById("table")
+  table.innerHTML = ""
+  document.getElementById("section").style.visibility = "visible"
   for (var i = 0; i < adicionarConvidado.length; i++) {
     table.innerHTML += `
    <tbody>
     <tr>
-      <th scope="row">${i+1}</th>
+      <th scope="row">${i + 1}</th>
       <td>${adicionarConvidado[i].nome}</td>
       <td>${adicionarConvidado[i].idade}</td>
       <td>${adicionarConvidado[i].email}</td>
-      <td><img src="./img/excluir.svg" alt="Botão Excluir" title="Excluir Registro" height="25" width="25">
+      <td><img src="./img/excluir.svg" onclick="excluir()" alt="Botão Excluir" title="Excluir Registro" height="25" width="25">
       <img src="./img/edit.svg" alt="Botão Editar" title="Editar Registro" height="25" width="25"></td></td>
     </tr>
   </tbody>
     `
   }
 
+}
+
+function excluir() {
+
+  for (var i = 0; i < adicionarConvidado.length; i++) {
+    adicionarConvidado.pop(adicionarConvidado[i])
+    document.getElementById("table").deleteRow(i)
+    
+  }
+  ;
 }
